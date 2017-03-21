92695a59c225c93cc448036d20ae77bb94eb1a2d
Tran Hau <hauvipapro@gmail.com>
2017-03-21 09:57:55 +0700

    Revert "Thuc hanh Tuan 1"
    This reverts commit ecf5101d05844b1882f0ee04aa108b8ff2470c2f.

--- LyThuyet-T3/Bai 5/ham.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-typedef void(*so_sanh)(int *, int *); // tang dan, giam dan
-
-void tang_dan(int *a, int *b);
-
-void giam_dan(int *a, int *b);
-
-void sap_xep(int *A, int so_luong, so_sanh T);
