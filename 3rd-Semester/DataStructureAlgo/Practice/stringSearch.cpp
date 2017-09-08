291ea2c4204b726f0fc973ceb20e1d55524e0cfe
Tran Hau <hauvipapro@gmail.com>
2017-09-08 22:09:09 +0000

    add brute string search

--- 3rd-Semester/DataStructureAlgo/Practice/stringSearch.cpp
+++ 3rd-Semester/DataStructureAlgo/Practice/stringSearch.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
-#include <string>
-
-// giver string text and string word
-// find the first occurrence of word in text
-
-char *bruteSearch(char *text, char *word);
 
 using namespace std;
 
-int main() { return 0; }
+// giver string text and string word
+// find the first occurrence of word in text
+int bruteSearch(char *text, char *word) {
+    int cur = 0;
+    bool flag;
+    while (text[cur])  // go through text
+    {
+        flag = true;  // reset flag for every index
+        for (int i = cur; text[i] & word[i - cur]; i++) {
+            if (text[i] != word[i - cur])
+                flag = false;  // 1 error make difference
+        }
+        if (flag) return cur;
+        cur++;
+    }
+    return -1;
+}
+
+using namespace std;
+
+int main() {
+    char text[] = "abc123abc123";
+    char word[] = "123";
+    int i = bruteSearch(text, word);
+    cout << i << endl;
+    return 0;
+}
