84c68cf6e418945dbf742e6d52535f8f0ed2ec1d
Tran Hau <hauvipapro@gmail.com>
2017-09-08 21:16:42 +0700

    init string search

---
+++ 3rd-Semester/DataStructureAlgo/Practice/stringSearch.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+// giver string text and string word
+// find the first occurrence of word in text
+
+char *bruteSearch(char *text, char *word);
+
+using namespace std;
+
+int main() { return 0; }
