35263577363071eb67f207f1dc9ee5b616aa8ef4
Tran Hau <hauvipapro@gmail.com>
2017-09-11 14:31:02 +0000

    fix to brute

--- 3rd-Semester/DataStructureAlgo/Practice/stringSearch.cpp
+++ 3rd-Semester/DataStructureAlgo/Practice/stringSearch.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <iostream>
 
 using namespace std;
@@ -5,9 +6,9 @@ using namespace std;
 // giver string text and string word
 // find the first occurrence of word in text
 int bruteSearch(char *text, char *word) {
-    int cur = 0;
     bool flag;
-    while (text[cur])  // go through text
+    for (int cur = 0; cur <= strlen(text) - strlen(word);
+         cur++)  // go through text
     {
         flag = true;  // reset flag for every index
         for (int i = cur; text[i] & word[i - cur]; i++) {
@@ -15,15 +16,17 @@ int bruteSearch(char *text, char *word) {
                 flag = false;  // 1 error make difference
         }
         if (flag) return cur;
-        cur++;
     }
     return -1;
 }
 
+// Use KMP algorithm
+int KMPSearch(char *text, char *word) { return 0; }
+
 using namespace std;
 
 int main() {
-    char text[] = "abc123abc123";
+    char text[] = "abc23a123b123c123";
     char word[] = "123";
     int i = bruteSearch(text, word);
     cout << i << endl;
