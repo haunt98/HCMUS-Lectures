e30acebad696e326fd527efe061719511283a614
Tran Hau <hauvipapro@gmail.com>
2017-09-11 16:19:08 +0000

    Update

--- 3rd-Semester/DataStructureAlgo/Practice/stringSearch.cpp
+++ 3rd-Semester/DataStructureAlgo/Practice/stringSearch.cpp
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -20,15 +21,71 @@ int bruteSearch(char *text, char *word) {
     return -1;
 }
 
-// Use KMP algorithm
-int KMPSearch(char *text, char *word) { return 0; }
+// longest proper prefix for word
+vector<int> fixWord(char *word) {
+    vector<int> temp(strlen(word));
+    temp[0] = 0;  // the first always 0
 
-using namespace std;
+    int i = 1;    // run from 1
+    int len = 0;  // len of longest prefix
+
+    while (i < temp.size()) {
+        if (word[i] == word[len]) {
+            len++;
+            temp[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = temp[len - 1];  // somehow I don't understand through
+                // do not increment i here
+            } else {
+                temp[i] = 0;
+                i++;
+            }
+        }
+    }
+    return temp;
+}
+
+// Use KMP algorithm
+int KMPSearch(char *text, char *word) {
+    bool flag;
+    vector<int> prefixWord = fixWord(word);
+    int i = 0;  // for text
+    int j = 0;  // for word
+    while (i < strlen(text)) {
+        if (word[j] == text[i]) {
+            i++, j++;
+        } else if (j == strlen(word)) {
+            return i - j;  // end at j so
+        }
+        // mismatch after j matched
+        else if (i < strlen(word) && word[j] != text[i]) {
+            if (j != 0) {
+                j = prefixWord[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return -1;
+}
+
+// print vector int
+void printVect(vector<int> temp) {
+    for (int i = 0; i < temp.size(); i++) {
+        cout << temp[i] << " ";
+    }
+    cout << endl;
+}
 
 int main() {
     char text[] = "abc23a123b123c123";
     char word[] = "123";
-    int i = bruteSearch(text, word);
-    cout << i << endl;
+
+    int i_b = bruteSearch(text, word);
+    cout << i_b << endl;
+    int i_kmp = KMPSearch(text, word);
+    cout << i_kmp << endl;
     return 0;
 }
