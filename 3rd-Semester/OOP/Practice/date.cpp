b7559648e98276359ef8361d11e99dc0dfd9d140
Tran Hau <hauvipapro@gmail.com>
2017-09-14 22:37:51 +0700

    change

--- 3rd-Semester/OOP/Practice/date.cpp
+++ 3rd-Semester/OOP/Practice/date.cpp
@@ -6,10 +6,16 @@ class Date {
     int y, m, d;
 
    public:
-    Date(int y, int m, int d);
+    Date(int yy, int mm, int dd) {}
     void addDay(int n);
-    int year { return y; }
-    int month{return m};
+    int year() { return y; }
+    int month() { return m; }
 };
+
+Date::Date(int yy, int mm, int dd) : y{yy}, m{mm}, d{dd};  // member init
+
+void Date::addDay(int n) {
+    // add n day
+}
 
 int main() { return 0; }
