1641ce1e950d4508e0cec84bd92cc25339dd2ba4
Tran Hau <hauvipapro@gmail.com>
2017-10-08 17:34:09 +0700

    bai 4 ly thuyet tuan 4

---
+++ 3rd-Semester/OOP/LyThuyet-T4/Bai-4/Time.h
@@ -0,0 +1,50 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+using namespace std;
+
+class Time
+{
+private:
+    int m_absSecond;
+
+public:
+    // khoi tao
+    Time() : m_absSecond(0) {}
+    Time(int hour, int minute, int second);
+    Time(int absSecond);
+    Time(const Time &clock);
+
+    // lay gio, phut giay tuong doi
+    int getHour() const;
+    int getMinute() const;
+    int getSecond() const;
+
+    // nghiep vu
+    int khoangCach(const Time &clock);
+    Time addHour(int hour);
+    Time addMinute(int minute);
+    Time addSecond(int minute);
+
+    // toan tu
+    bool operator>(const Time &clock);
+    bool operator<(const Time &clock);
+    bool operator==(const Time &clock);
+    bool operator>=(const Time &clock);
+    bool operator<=(const Time &clock);
+    bool operator!=(const Time &clock);
+
+    // cong tru giay
+    Time operator+(const int &second);
+    Time operator-(const int &second);
+    Time &operator++();
+    Time operator++(int);
+    Time &operator--();
+    Time operator--(int);
+
+    friend istream &operator>>(istream &in, Time &clock);
+    friend ostream &operator<<(ostream &out, const Time &clock);
+};
+
+#endif
---
+++ 3rd-Semester/OOP/LyThuyet-T4/Bai-4/main.cpp
@@ -0,0 +1,9 @@
+#include "Time.h"
+
+int main()
+{
+    Time a;
+    cin >> a;
+    cout << (a - 1000) << endl;
+    return 0;
+}
