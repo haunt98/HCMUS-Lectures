bc4cfec9f2e7587313d22715f4f415dc6de86def
Tran Hau <hauvipapro@gmail.com>
2017-10-13 22:52:12 +0700

    bai 3 ly thuyet tuan 6

---
+++ 3rd-Semester/OOP/LyThuyet-T6/Bai-3/main.cpp
@@ -0,0 +1,20 @@
+#include "Xe.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    XeMay a;
+    a.themHang(10);
+    a.themXang(200);
+    a.chay(100);
+    cout << a.xang() << endl;
+    XeTai b;
+    b.themHang(1000);
+    b.themXang(100);
+    b.chay(1000);
+    cout << b.hetXang() << endl;
+    cout << b.xang() << endl;
+    return 0;
+}
