9ce1f9db6d20f979e9a2a0a7472185f0713929fa
Tran Hau <hauvipapro@gmail.com>
2017-10-21 15:58:44 +0700

    chap 1 effective c++

--- 3rd-Semester/OOP/Practice/dontinclude.cpp
+++ 3rd-Semester/OOP/Practice/dontinclude.cpp
@@ -19,9 +19,32 @@ private:
 
 const int NumTurns = 5; // define
 
+class TextBlock
+{
+private:
+    string m_text;
+
+public:
+    TextBlock(string text) { m_text = text; }
+    const char &operator[](size_t position) const { return m_text[position]; }
+    char &operator[](size_t position) { return m_text[position]; }
+};
+
 int main()
 {
     cout << Name << endl;
     cout << AnotherName << endl;
+
+    TextBlock tb("Hello");
+    cout << tb[0] << endl;
+    tb[0] = 't';
+    cout << tb[0] << endl;
+    const TextBlock ctb("Word");
+    cout << ctb[0] << endl;
+    // ctb[0]='h'; error
+
+    const char *const text = "a string";
+    cout << text << endl;
+
     return 0;
 }
