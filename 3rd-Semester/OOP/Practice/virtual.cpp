edcb6f30d9592cf6aa484e2bfeefc38531539066
Tran Hau <hauvipapro@gmail.com>
2017-10-25 23:58:39 +0700

    learn basic virtual: must virtual dto, and alwasy use virtual with pointer

---
+++ 3rd-Semester/OOP/Practice/virtual.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+    virtual void f1()
+    {
+        cout << "A::f1" << endl;
+        f2();
+    }
+    virtual void f2() { cout << "A::f2" << endl; }
+    virtual ~A() {}
+};
+
+class B : public A
+{
+public:
+    void f1()
+    {
+        cout << "B::f1" << endl;
+        f2();
+    }
+    void f2() { cout << "B::f2" << endl; }
+};
+
+int main()
+{
+    A *p = new B;
+    // p kieu A nhung giu dia chi cua B
+    p->f1();
+    delete p;
+    return 0;
+}
