32eb4fc5c117b87cb60c1fffe7c3a49e04163fa7
Tran Hau <hauvipapro@gmail.com>
2017-12-12 18:03:16 +0700

    bai 1 cs274

---
+++ 3rd-Semester/OOP/CuoiKy-CS274/Bai-1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Base
+{
+public:
+    Base() { cout << "Base" << endl; }
+    Base(int i) { cout << "Base" << i << endl; }
+    ~Base() { cout << "Destruct Base" << endl; }
+};
+
+class Der : public Base
+{
+public:
+    Der() { cout << "Der" << endl; }
+    Der(int i) : Base(i) { cout << "Der" << i << endl; }
+    ~Der() { cout << "Destruct Der" << endl; }
+};
+int main()
+{
+    Base a;
+    Der d(2);
+    return 0;
+}
+
+// When call dto of Der, call dto of Base
+// When we need to use virtual in dto is
+// Base *b = new Der(3);
+// delete b;
