fe70363801f1b12295f7735081d7e1b3eb3069fe
Tran Hau <hauvipapro@gmail.com>
2018-01-07 16:23:26 +0700

    add merge 2 arr and fix typo

---
+++ 4_CTDL_GT/SmallProgram/sort_search/merge.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void merge(int A[], int n, int B[], int m);
+
+int main()
+{
+	int A[20] = {1, 3, 5, 7, 9, 11};
+	int B[] = {2, 6};
+
+	merge(A, 6, B, 2);
+	for (int i = 0; i < 8; ++i) {
+		printf("%d\n", A[i]);
+	}
+	return 0;
+}
+
+// A has enough buffer for B
+void merge(int A[], int n, int B[], int m)
+{
+	// assume A[0..m+n-1]
+	int x = n - 1;
+	int y = m - 1;
+	for (int i = m + n - 1; i >= 0; --i) {
+		if (A[x] > B[y])
+			A[i] = A[x--];
+
+		else
+			A[i] = B[y--];
+	}
+}
