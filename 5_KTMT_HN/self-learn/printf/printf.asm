69bc34096a15ae805d9455458659e4f26549a750
Tran Hau <hauvipapro@gmail.com>
2018-01-17 22:03:02 +0700

    finally printf inside asm

--- 5_KTMT_HN/self-learn/printf/printf.asm
+++ 5_KTMT_HN/self-learn/printf/printf.asm
@@ -1,15 +1,21 @@
 ; use printf in C library
 
-        global  main
         extern  printf
 
         section .data
-fmt     db      "%s", 10, 0
+fmt     db      "%s", 10, 0     ; 10 is "\n", 0 is "\0"
 msg     db      "printf haha", 0
 
         section .text
+        global  main
 main:
+        push    rbp     ; set up stack frame
+
         mov     rdi, fmt
         mov     rsi, msg
+        mov     rax, 0
         call    printf
+
+        pop     rbp     ; restore stack
+        mov     rax, 0  ; no error, return value
         ret
