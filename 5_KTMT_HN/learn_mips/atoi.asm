910b844fe96bdaf42bf6ccffdd750c6963be9b85
Tran Hau <hauvipapro@gmail.com>
2018-04-01 00:06:44 +0700

    convert atoi to function

--- 5_KTMT_HN/learn_mips/atoi.asm
+++ 5_KTMT_HN/learn_mips/atoi.asm
@@ -12,45 +12,52 @@ main:
         add $a1, $zero, 1024    # length
         syscall
 
-        # store result in $t0
-        add $t0, $zero, $zero
+        # call atoi
+        jal atoi
 
-        # $t1 is pointer to str
-        la $t1, str
+        # print result
+        add $a0, $zero, $v0
+        add $v0, $zero, 1       # syscall print int
+        syscall
+
+        # exit
+        add $v0, $zero, 10      # syscall exit
+        syscall
+
+atoi:
+        # store result in $v0
+        add $v0, $zero, $zero
+
+        # $t1 is pointer to str, init $a0
+        add $t1, $zero, $a0
 
         # get sign (negative or positive)
         lb $t2, 0($t1)
         addi $t3, $zero, 1
-        bne $t2, 45, sum_loop   # 45 is '-'
+        bne $t2, 45, atoi_sum_loop      # 45 is '-'
         addi $t3, $zero, -1
         addi $t1, $t1, 1        # skip '-'
 
-sum_loop:
+atoi_sum_loop:
         lb $t2, 0($t1)          # load character in 1 byte
         slt $t4, $t2, 48        # check str[i] >= '0'
-        bne $t4, $zero, exit
+        bne $t4, $zero, atoi_exit
         slt $t4, $t2, 58        # check str[i] <= '9'
-        beq $t4, $zero, exit
+        beq $t4, $zero, atoi_exit
 
         # result = result * 10 + str[i] - '0'
         addi $t4, $zero, 10
-        mult $t0, $t4           # result * 10
-        mflo $t0                # get from LO
+        mult $v0, $t4           # result * 10
+        mflo $v0                # get from LO
 
         addi $t2, $t2, -48      # str[i] - '0'
-        add $t0, $t0, $t2
+        add $v0, $v0, $t2
         addi $t1, $t1, 1
-        j sum_loop
+        j atoi_sum_loop
 
-exit:
+atoi_exit:
         # use sign
-        mult $t0, $t3
-        mflo $t0
+        mult $v0, $t3
+        mflo $v0
 
-        # print result
-        add $v0, $zero, 1       # syscall print int
-        add $a0, $zero, $t0
-        syscall
-
-        add $v0, $zero, 10      # syscall exit
-        syscall
+        jr $ra
