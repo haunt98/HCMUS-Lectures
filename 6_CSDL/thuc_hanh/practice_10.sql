18b21be362b7850693483b897cb2e1832bcb256d
Tran Hau <hauvipapro@gmail.com>
2018-06-07 19:30:35 +0700

    more sql practice

--- 6_CSDL/thuc_hanh/practice_10.sql
+++ 6_CSDL/thuc_hanh/practice_10.sql
@@ -11,8 +11,119 @@ union
 from giaovien gv join bomon bm on gv.magv = bm.truongbm
 where year(bm.NGAYNHANCHUC)> 1995 );
 
--- Cho  biết  tên  những  giáo  viên  khoa  Công  nghệ  thông  tin. 
+--Cho  biết  tên  những  giáo  viên  khoa  Công  nghệ  thông  tin. 
 select gv.hoten
 from giaovien gv join bomon bm on gv.mabm = bm.mabm
 	join khoa k on bm.makhoa = k.makhoa
 where k.tenkhoa = N'Công nghệ thông tin';
+
+--Cho  biết  thông  tin  của  bộ  môn  cùng  thông  tin  giảng  viên  làm  trưởng  bộ  môn  đó. 
+select *
+from bomon bm left join giaovien gv on bm.TRUONGBM = gv.MAGV
+
+--Với  mỗi  giáo  viên,  hãy  cho  biết  thông  tin  của  bộ  môn  mà  họ  đang  làm  việc. 
+select *
+from GIAOVIEN gv left join BOMON bm on gv.MABM = bm.MABM
+
+--Cho  biết  tên  người  chủ  nhiệm  đề  tài  của  những  đề  tài  thuộc  chủ  đề  Quản  lý  giáo  dục. 
+select gv.*
+from GIAOVIEN gv join DETAI d on gv.magv = d.MADT 
+	join CHUDE c on d.MACD = c.MACD
+where c.TENCD = N'Quản lý giáo dục'
+
+--Cho  biết  số  lượng  giáo  viên  viên  và  tổng  lương  của  họ. 
+select count(*), sum(LUONG)
+from GIAOVIEN
+
+--Cho  biết  số  lượng  giáo  viên  và  lương  trung  bình  của  từng  bộ  môn. 
+select mabm, count(magv), avg(LUONG)
+from GIAOVIEN gv
+group by gv.MABM
+
+--Cho  biết  tên  chủ  đề  và  số  lượng  đề  tài  thuộc  về  chủ  đề  đó. 
+select c.TENCD, count(d.MADT)
+from DETAI d join CHUDE c on d.MACD = c.MACD
+group by d.macd, c.TENCD
+
+--Cho  biết  tên  giáo  viên  và  số  lượng  đề  tài  mà  giáo  viên  đó  tham  gia. 
+select gv.HOTEN, count(distinct t.MADT)
+from giaovien gv left join THAMGIADT t on gv.MAGV = t.MAGV
+group by gv.MAGV, gv.HOTEN
+
+--Cho  biết  tên  giáo  viên  và  số  lượng  đề  tài  mà  giáo  viên  đó  làm  chủ  nhiệm.
+select gv.HOTEN, count(distinct d.MADT)
+from giaovien gv left join DETAI d on gv.MAGV = d.GVCNDT
+group by gv.MAGV, gv.HOTEN
+
+--Cho  biết  tên  những  giáo  viên  đã  tham  gia  từ  1  đề  tài  trở  lên. 
+select gv.HOTEN
+from giaovien gv join THAMGIADT t on gv.MAGV = t.MAGV
+group by gv.MAGV, gv.HOTEN
+having count(distinct t.MADT) >= 1
+
+--Cho  biết  mức  lương  cao  nhất  của  các  giảng  viên.
+select max(LUONG)
+from GIAOVIEN
+
+select distinct g.LUONG
+from GIAOVIEN g
+where g.LUONG >= all (
+	select g2.LUONG
+	from GIAOVIEN g2)
+
+--Cho  biết  những  giáo  viên  có  lương  lớn  nhất.
+select g.*
+from GIAOVIEN g
+where g.LUONG >= all (
+	select g2.LUONG
+	from GIAOVIEN g2)
+
+--Cho  biết  tên  những  giáo  viên  khoa  Công  nghệ  thông  tin  mà  chưa  tham  gia  đề  tài  nào.
+select g.HOTEN
+from GIAOVIEN g join BOMON b on g.MABM = b.MABM
+	join KHOA k on b.MAKHOA = k.MAKHOA
+where k.TENKHOA = N'Công nghệ thông tin' and g.MAGV not in (
+	select magv
+	from THAMGIADT)
+
+--Tìm  những  trưởng  bộ  môn  tham  gia  tối  thiểu  1  đề  tài 
+select distinct g.HOTEN
+from GIAOVIEN g join BOMON b on g.MAGV = b.TRUONGBM
+	join THAMGIADT t on g.MAGV = t.MAGV
+
+--Cho  biết  tên  giáo  viên  nào  mà  tham  gia  đề  tài  đủ  tất  cả  các  chủ  đề. 
+select g.HOTEN
+from GIAOVIEN g 
+where not exists(
+	(select distinct c.MACD 
+	from CHUDE c)
+	except
+	(select distinct d2.MACD
+	from GIAOVIEN g2 join THAMGIADT t2 on g2.MAGV = t2.MAGV
+		join DETAI d2 on t2.MADT = d2.MADT
+	where g2.MAGV = g.MAGV)
+)
+
+select g.HOTEN 
+from GIAOVIEN g
+where not exists(
+	select *
+	from CHUDE c
+	where not exists(
+		select *
+		from GIAOVIEN g2 join THAMGIADT t on g2.MAGV = t.MAGV
+			join DETAI d on t.MADT = d.MADT
+		where d.MACD = c.MACD
+	)
+)
+
+select g.HOTEN
+from GIAOVIEN g join THAMGIADT t on g.MAGV = t.MAGV
+	join DETAI d on t.MADT = d.MADT
+group by g.HOTEN, g.MAGV
+having count(distinct d.MACD) = (
+	select count(distinct macd)
+	from CHUDE
+)
+
+--Cho  biết  tên  đề  tài  nào  mà  được  tất  cả  các  giáo  viên  của  bộ  môn  HTTT  tham  gia. 
---
+++ 6_CSDL/thuc_hanh/practice_11.sql
@@ -0,0 +1,122 @@
+﻿use QuanLyChuyenBay;
+go
+--Với  mỗi  sân  bay  (SBDEN),  cho  biết  số  lượng  chuyến  bay  hạ  cánh  xuống  sân  bay  đó.  Kết 
+--quả  được  sắp  xếp  theo  thứ  tự  tăng  dần  của  sân  bay  đến. 
+select SBDEN, count(MACB)
+from CHUYENBAY
+group by SBDEN
+order by SBDEN
+
+--Với  mỗi  sân  bay  (SBDI),  cho  biết  số  lượng  chuyến  bay  xuất  phát  theo  từng  ngày.  Xuất 
+--ra  mã  sân  bay  đi,  ngày  và  số  lượng.
+select c.SBDI, l.NGAYDI, count(c.MACB)
+from CHUYENBAY c join LICHBAY l on c.MACB = l.MACB
+group by c.SBDI, l.NGAYDI
+order by c.SBDI
+
+--Với  mỗi  lịch  bay,  cho  biết  mã  chuyến  bay,  ngày  đi  cùng  với  số  lượng  nhân  viên  không 
+--phải  là  phi  công  của  chuyến  bay  đó. 
+select l.MACB, l.NGAYDI, count (p.manv)
+from lichbay l join PHANCONG p on (l.MACB = p.MACB and l.NGAYDI = p.NGAYDI)
+	join NHANVIEN n on p.manv = n.MANV
+where n.LOAINV = 0
+group by l.macb, l.NGAYDI
+
+--Cho  biết  tên  phi  công  được  phân  công  vào  ít  nhất  2  chuyến  bay  trong  cùng  một  ngày. 
+select n.TEN
+from PHANCONG p join LICHBAY l on (p.NGAYDI = l.NGAYDI and p.MACB = l.MACB)
+	join NHANVIEN n on p.MANV = n.MANV
+where n.LOAINV = 1
+group by n.TEN, p.NGAYDI
+having count(p.MACB) >= 2
+
+--Cho  biết  mã  chuyến  bay  và  ngày  đi  của  những  chuyến  bay  có  ít  hơn  3  hành  khách  đặt 
+--chỗ.
+select l.MACB, l.NGAYDI
+from LICHBAY l join DATCHO d on l.NGAYDI = d.NGAYDI and l.MACB = d.MACB
+group by l.NGAYDI, l.MACB
+having count(d.MAKH) < 3
+
+--Cho  biết  số  hiệu  máy  bay  và  loại  máy  bay  mà  phi  công  có  mã  1001  được  phân  công  lái 
+--trên  2  lần.
+select l.MALOAI, l.SOHIEU
+from PHANCONG p join LICHBAY l on p.NGAYDI = l.NGAYDI and p.MACB = l.MACB
+where p.MANV = '1001'
+group by l.MALOAI, l.SOHIEU
+having count(*) > 2
+
+--Với  mỗi  hãng  sản  xuất,  cho  biết  số  lượng  loại  máy  bay  mà  hãng  đó  đã  sản  xuất.  Xuất  ra 
+--hãng  sản  xuất  và  số  lượng.
+select HANGSX, count(*)
+from LOAIMB
+group by HANGSX
+
+--Cho  biết  hãng  sản  xuất,  mã  loại  và  số  hiệu  của  máy  bay  đã  được  sử  dụng  nhiều  nhất.
+select m.MALOAI, m.SOHIEU, l.HANGSX, count(*)
+from maybay m join LOAIMB l on m.MALOAI = l.MALOAI
+	join LICHBAY lb on m.MALOAI = lb.MALOAI and m.SOHIEU = lb.SOHIEU
+group by m.MALOAI, m.SOHIEU, l.HANGSX
+having count(*) >= all (
+	select count(*)
+	from maybay m2 join LOAIMB l2 on m2.MALOAI = l2.MALOAI
+	join LICHBAY lb2 on m2.MALOAI = lb2.MALOAI and m2.SOHIEU = lb2.SOHIEU
+	group by m2.MALOAI, m2.SOHIEU, l2.HANGSX
+)
+
+--Cho  biết  tên  nhân  viên  được  phân  công  đi  nhiều  chuyến  bay  nhất. 
+select n.TEN
+from PHANCONG p join NHANVIEN n on p.MANV = n.MANV
+group by p.manv, n.TEN
+having count(*) >= all (
+	select count(*)
+	from PHANCONG p2 join NHANVIEN n2 on p2.MANV = n2.MANV
+	group by p2.manv, n2.TEN
+)
+
+--Cho  biết  sân  bay  (SBDEN)  và  số  lượng  chuyến  bay  của  sân  bay  có  ít  chuyến  bay  đáp 
+--xuống  nhất. 
+select c.SBDEN, count(*)
+from chuyenbay c
+group by c.SBDEN
+having count(*)	>= all (
+	select count(*)
+	from CHUYENBAY c2
+	group by c2.SBDEN
+)
+
+--Cho  biết  mã  số,  tên  và  lương  của  các  phi  công  có  khả  năng  lái  nhiều  loại  máy  bay  nhất.
+select n.MANV, n.TEN, n.LUONG
+from NHANVIEN n join KHANANG k on n.MANV = k.MANV
+where n.LOAINV = 1
+group by n.MANV, n.TEN, n.LUONG
+having count(*) >= all (
+	select count(*)
+	from KHANANG k2
+	group by k2.MANV
+)
+
+--Cho  biết  tên,  địa  chỉ  của  các  nhân  viên  có  lương  cao  nhất  trong  phi  hành  đoàn  (các 
+--nhân  viên  được  phân  công  trong  một  chuyến  bay)  mà  người  đó  tham  gia.
+select *
+from NHANVIEN n join PHANCONG p on n.MANV = p.MANV
+where n.LUONG >= all (
+	select n2.LUONG 
+	from NHANVIEN n2 join PHANCONG p2 on n2.MANV = p2.MANV
+	where p.NGAYDI = p2.NGAYDI and p.MACB = p2.MACB
+)
+
+--Cho  biết  mã  chuyến  bay,  giờ  đi  và  giờ  đến  của  chuyến  bay  bay  sớm  nhất  trong  ngày. 
+select *
+from CHUYENBAY c
+where c.GIODI <= all (
+	select c2.GIODI
+	from CHUYENBAY c2
+)
+
+--Với  mỗi  chuyến  bay  có  trên  3  hành  khách,  cho  biết  mã  chuyến  bay  và  số  lượng  nhân 
+--viên  trên  chuyến  bay  đó.  Xuất  ra  mã  chuyến  bay  và  số  lượng  nhân  viên.
+select l.MACB, count(distinct p.manv)
+from LICHBAY l join DATCHO d on (l.NGAYDI = d.NGAYDI and l.MACB = d.MACB)
+	join PHANCONG p on l.NGAYDI = p.NGAYDI and l.MACB = p.MACB
+group by l.NGAYDI, l.MACB
+having count(distinct d.MAKH) > 3
