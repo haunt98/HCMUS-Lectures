b376e144e2601a73684e2ff437ab5c94a943909c
Tran Hau <hauvipapro@gmail.com>
2018-06-17 00:22:19 +0700

    laste update for semester 4 :(

--- 6_CSDL/thuc_hanh/practice_10.sql
+++ 6_CSDL/thuc_hanh/practice_10.sql
@@ -126,4 +126,88 @@ having count(distinct d.MACD) = (
 	from CHUDE
 )
 
---Cho  biết  tên  đề  tài  nào  mà  được  tất  cả  các  giáo  viên  của  bộ  môn  HTTT  tham  gia. 
+--Cho  biết  tên  đề  tài  nào  mà  được  tất  cả  các  giáo  viên  của  bộ  môn  HTTT  tham  gia.
+select d.TENDT
+from  DETAI d
+where not exists(
+	(select distinct g.magv 
+	from GIAOVIEN g
+	where g.mabm = 'HTTT')
+	except
+	(select distinct g2.MAGV
+	from giaovien g2 join THAMGIADT t on g2.MAGV = t.MAGV
+	where t.MADT = d.MADT)
+)
+
+select d.TENDT
+from  DETAI d
+where not exists(
+	select *
+	from GIAOVIEN g
+	where g.MABM = 'HTTT' and not exists(
+		select *
+		from THAMGIADT t 
+		where t.MAGV = g.MAGV and t.MADT = d.MADT
+	)
+)
+
+select d.TENDT
+from DETAI d join THAMGIADT t on d.MADT = t.MADT
+	join GIAOVIEN g on g.MAGV = t.MAGV
+where g.MABM = 'HTTT'
+group by d.TENDT, d.MADT
+having count(distinct g.MAGV) = (
+	select count(distinct magv)
+	from GIAOVIEN
+	where MABM = 'HTTT'
+)
+
+--Cho  biết  giáo  viên  nào  đã  tham  gia  tất  cả  các  đề  tài  có  mã  chủ  đề  là  QLGD. 
+select g.HOTEN
+from giaovien g
+where not exists (
+	(select distinct MADT
+	from DETAI
+	where MACD = 'QLGD')
+	except
+	(select distinct d.MADT
+	from THAMGIADT t join DETAI d on t.MADT = d.MADT
+	where t.MAGV = g.MAGV and d.MACD = 'QLGD')
+)
+
+--Cho  biết  tên  giáo  viên  nào  mà  tham  gia  tất  cả  các  công  việc  của  đề  tài  006.
+select g.HOTEN
+from GIAOVIEN g
+where not exists(
+	(select madt, SOTT
+	from CONGVIEC
+	where MADT = '006')
+	except
+	(select t.MADT, t.STT
+	from GIAOVIEN g2 join THAMGIADT t on g2.MAGV = t.MAGV
+	where t.MADT = '006' and g.MAGV = g2.MAGV
+	)
+)
+
+select g.HOTEN 
+from GIAOVIEN g
+where not exists(
+	select *
+	from CONGVIEC c
+	where c.MADT = '006' and not exists (
+		select *
+		from THAMGIADT t
+		where t.MAGV = g.MAGV and t.MADT = c.MADT and t.STT = c.SOTT
+	)
+)
+
+select g.HOTEN
+from GIAOVIEN g join THAMGIADT t on g.MAGV = t.MAGV
+where t.MADT = '006'
+group by g.HOTEN, g.MAGV
+having count(distinct t.STT) = (
+	select count(*)
+	from CONGVIEC
+	where MADT = '006'
+)
+
